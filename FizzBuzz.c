7123cc36579c4259987dbe113edd9d62dd0df2cd
Gagesmith2299 <31717700+Gagesmith2299@users.noreply.github.com>
2019-01-25 22:04:47 -0600

    Implimented FizzBuzz working program

---
+++ FizzBuzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void)
+{
+int i=1;
+
+	for(i=1; i<=100; i++)
+	{
+		if(i%15==0)
+		printf("FIZZBUZZ\n");
+
+	else if((i%3)==0)
+		printf("FIZZ\n");
+
+	else if ((i%5)==0)
+		printf("BUZZ\n");
+
+	else
+		printf("%d\n, i);
+	}
+	return 0;
+}
+
